942e2010db5076de7d3c053f9c26662fe1ef4f08
danilomartins <dglmartins@outlook.com>
2017-10-08 16:05:10 -0400

    Change title in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logoPath} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React with Electron, ready for Redux, working with es6, class arrow function and spread syntax compilation on the fly.</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
