4a17540bfa1d444d93e62c2bd9d3a15ed2ca9ac5
danilomartins <dglmartins@outlook.com>
2017-10-08 16:03:04 -0400

    Change App.js to mimic create-react-app first page, add and link css files to index.html, add react logo and link to App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class App extends Component {
 
   render() {
+    const logoPath = './src/logo.svg'
     return (
-      <div>
-        Hello World!
+      <div className="App">
+        <header className="App-header">
+          <img src={logoPath} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
